7ee9eb1a0549c4b00c81182f30b4cc052a15416e
yevgeny <hongyevgeny@gmail.com>
2021-07-21 22:51:51 +0900

    update logic

--- cli.js
+++ cli.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const trade = require("./trade.js")
 const args = process.argv.slice(2)
 
-console.log(args)
+// console.log(args)
 
 switch(args[0]){
     case 'set_timeout':
@@ -10,21 +10,19 @@ switch(args[0]){
         process.exit()
     case 'set_private_key':
         fs.writeFileSync("private_key", args[1])
-        console.log(fs.readFileSync('private_key').toString())
         process.exit()
     case 'set_wallet':
         fs.writeFileSync("wallet", args[1])
         process.exit()
     case 'buy':
-        var private_key = fs.readFileSync('private_key').toString();
-        trade.setWallet(private_key)
         break;
 }
 
-
 var wallet = fs.readFileSync("wallet").toString();
 var timeout = parseFloat(fs.readFileSync("timeout").toString());
+var private_key = fs.readFileSync('private_key').toString();
 
+trade.setWallet(private_key)
 trade.setTimeout(timeout);
 
 buy = async () => {

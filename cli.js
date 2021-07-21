d89ca225fbfdfd8df0914173c59be94c190dd7fb
yevgeny <hongyevgeny@gmail.com>
2021-07-21 22:48:28 +0900

    update command
    mistake wallet and private_key
    and add timeout

--- cli.js
+++ cli.js
@@ -5,38 +5,41 @@ const args = process.argv.slice(2)
 console.log(args)
 
 switch(args[0]){
+    case 'set_timeout':
+        fs.writeFileSync("timeout", args[1])
+        process.exit()
+    case 'set_private_key':
+        fs.writeFileSync("private_key", args[1])
+        console.log(fs.readFileSync('private_key').toString())
+        process.exit()
     case 'set_wallet':
         fs.writeFileSync("wallet", args[1])
-        console.log(fs.readFileSync('wallet').toString())
         process.exit()
-        break;
-    case 'set_recipient':
-        fs.writeFileSync("recipient", args[1])
-        process.exit()
-        break;
     case 'buy':
-        var wallet = fs.readFileSync('wallet').toString();
-        trade.setWallet(wallet)
+        var private_key = fs.readFileSync('private_key').toString();
+        trade.setWallet(private_key)
         break;
 }
 
 
-var recipient = fs.readFileSync("recipient").toString();
+var wallet = fs.readFileSync("wallet").toString();
+var timeout = parseFloat(fs.readFileSync("timeout").toString());
+
+trade.setTimeout(timeout);
 
 buy = async () => {
     console.log("start buy")
-    console.log(recipient)
+
     result = await trade.buy(
         args[1], // tokenOut
         args[2], // tokenIn
         parseFloat(args[3]), // amount
-        recipient, // recipient
+        wallet, // wallet
         15, // slippage
         5, // gasPrice
         450000, // gasPrice
         0.1 // minBnb
     )
-
 
     console.log(result)
     process.exit();
